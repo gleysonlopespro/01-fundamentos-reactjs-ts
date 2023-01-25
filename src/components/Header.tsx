e91c80cfce1c3a6f7f605f6c8bf447a314055658
Gleyson Lopes <97358393+gleysonlopespro@users.noreply.github.com>
2023-01-24 21:36:21 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import styles from './Header.module.css';
 
-import igniteLogo from '../ignite-logo.svg'
+import igniteLogo from './assets/ignite-logo.svg'
 
 export function Header(){
     return (
