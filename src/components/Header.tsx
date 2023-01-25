d35d9328d4162ff63d610a688c6b220ab61f944a
Gleyson Lopes <97358393+gleysonlopespro@users.noreply.github.com>
2023-01-24 21:46:27 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import styles from './Header.module.css';
 export function Header(){
     return (
         <header className={styles.header}>
-           <!-- <img src={igniteLogo} alt="LogoTipo Ignite" />-->
+           
         </header>
         
     );
